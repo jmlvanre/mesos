4e85cf42e5340662e406e9eebfe31a63752c5323
jmlvanre <joris.van.remoortere@gmail.com>
2014-09-29 14:30:10 -0700

    fix style issues.

--- 3rdparty/libprocess/src/tests/benchmarks.cpp
+++ 3rdparty/libprocess/src/tests/benchmarks.cpp
@@ -11,6 +11,20 @@
 #include <process/gtest.hpp>
 #include <process/process.hpp>
 
+using namespace process;
+
+using std::condition_variable;
+using std::function;
+using std::istringstream;
+using std::lock_guard;
+using std::mutex;
+using std::ostringstream;
+using std::string;
+using std::thread;
+using std::unique_lock;
+using std::unordered_set;
+using std::vector;
+
 int main(int argc, char** argv)
 {
   // Initialize Google Mock/Test.
@@ -26,9 +40,6 @@ int main(int argc, char** argv)
   return RUN_ALL_TESTS();
 }
 
-using namespace process;
-using std::string;
-
 class BenchmarkProcess : public Process<BenchmarkProcess>
 {
 public:
@@ -37,12 +48,12 @@ public:
       size_t _max_outstanding = 1,
       const Option<UPID>& _other = Option<UPID>())
       : other(_other),
-      counter(0UL),
-      done(false),
-      num_iter(_num_iter),
-      max_outstanding(_max_outstanding),
-      outstanding(0),
-      sent(0)
+        counter(0UL),
+        done(false),
+        num_iter(_num_iter),
+        max_outstanding(_max_outstanding),
+        outstanding(0),
+        sent(0)
   {
     if (other.isSome()) {
       setlink(other.get());
@@ -58,30 +69,29 @@ public:
     install("pong", &BenchmarkProcess::pong);
   }
 
-  void setlink(const UPID&that) {
+  void setlink(const UPID& that) {
     link(that);
   }
 
   void start() {
-    const char *msg = "hi";
+    const char* msg = "hi";
     for (;outstanding < max_outstanding &&
         sent < num_iter; ++outstanding, ++sent) {
       send(other.get(), "ping", msg, strlen(msg));
     }
-    std::unique_lock<std::mutex> lock(mutex);
+    unique_lock<mutex> lock(mut);
     while (!done) {
       cond.wait(lock);
     }
   }
 
 private:
-
   void ping(const UPID& from, const string& body) {
     if (linked_ports.find(from.port) == linked_ports.end()) {
       setlink(from);
       linked_ports.emplace(from.port);
     }
-    const char *msg = "hi";
+    const char* msg = "hi";
     send(from, "pong", msg, strlen(msg));
   }
 
@@ -89,11 +99,11 @@ private:
     ++counter;
     --outstanding;
     if (counter >= num_iter) {
-      std::lock_guard<std::mutex> lock(mutex);
+      lock_guard<mutex> lock(mut);
       done = true;
       cond.notify_one();
     }
-    const char *msg = "hi";
+    const char* msg = "hi";
     for (;outstanding < max_outstanding &&
         sent < num_iter; ++outstanding, ++sent) {
       send(other.get(), "ping", msg, strlen(msg));
@@ -105,16 +115,16 @@ private:
   size_t counter;
 
   bool done;
-  std::mutex mutex;
-  std::condition_variable cond;
+  mutex mut;
+  condition_variable cond;
 
   const size_t num_iter;
   const size_t max_outstanding;
   size_t outstanding;
   size_t sent;
-  std::unordered_set<int> linked_ports;
-
+  unordered_set<int> linked_ports;
 };
+
 
 TEST(Process, Process_BENCHMARK_Test)
 {
@@ -123,10 +133,10 @@ TEST(Process, Process_BENCHMARK_Test)
   const size_t num_threads = 8;
   const size_t num_proc = 4;
 
-  std::vector<int> out_pipe_vec;
-  std::vector<int> in_pipe_vec;
-  std::vector<pid_t> pid_vec;
-  std::function<void (size_t)> do_fork = [&](size_t more_to_launch) {
+  vector<int> out_pipe_vec;
+  vector<int> in_pipe_vec;
+  vector<pid_t> pid_vec;
+  function<void (size_t)> do_fork = [&](size_t more_to_launch) {
     // fork in order to get num_proc seperate ProcessManagers. This
     // avoids the short-circuit built into ProcessManager for
     // processes communicating in the same manager.
@@ -134,19 +144,23 @@ TEST(Process, Process_BENCHMARK_Test)
     pid_t pid = -1;
     if(pipe(pipes) < 0) {
       perror("pipe failed");
+      abort();
     }
     pid = fork();
 
     if (pid < 0) {
       perror("fork() failed");
+      abort();
     } else if (pid == 0) {
       // child
       int32_t strsize = 0;
       size_t r = read(pipes[0], &strsize, sizeof(strsize));
+      EXPECT_EQ(r, sizeof(strsize));
       char buf[strsize];
       memset(&buf, 0, strsize);
       r = read(pipes[0], &buf, strsize);
-      std::istringstream iss(buf);
+      EXPECT_EQ(r, strsize);
+      istringstream iss(buf);
       UPID other;
       iss >> other;
       auto launcher = [&]() {
@@ -158,7 +172,7 @@ TEST(Process, Process_BENCHMARK_Test)
       };
       Stopwatch watch;
       watch.start();
-      std::vector<std::thread> tvec;
+      vector<thread> tvec;
       for (size_t i = 0; i < num_threads; ++i) {
         tvec.emplace_back(launcher);
       }
@@ -168,7 +182,8 @@ TEST(Process, Process_BENCHMARK_Test)
       double elapsed = watch.elapsed().secs();
       size_t total_iter = num_threads * num_iter;
       size_t rpc_per_sec = total_iter / elapsed;
-      write(pipes[1], &rpc_per_sec, sizeof(rpc_per_sec));
+      r = write(pipes[1], &rpc_per_sec, sizeof(rpc_per_sec));
+      EXPECT_EQ(r, sizeof(rpc_per_sec));
       close(pipes[0]);
       exit(0);
     } else {
@@ -179,18 +194,21 @@ TEST(Process, Process_BENCHMARK_Test)
       if (more_to_launch <= 1) {
         BenchmarkProcess process(num_iter, queue_depth);
         UPID pid = spawn(&process);
-        std::ostringstream ss;
+        ostringstream ss;
         ss << pid;
         int32_t strsize = ss.str().size();
         for (auto fd : out_pipe_vec) {
           size_t w = write(fd, &strsize, sizeof(strsize));
+          EXPECT_EQ(w, sizeof(strsize));
           w = write(fd, ss.str().c_str(), strsize);
+          EXPECT_EQ(w, strsize);
           close(fd);
         }
         size_t total_rpcs_per_sec = 0;
         for (auto fd : in_pipe_vec) {
           size_t rpcs = 0;
           size_t r = read(fd, &rpcs, sizeof(rpcs));
+          EXPECT_EQ(r, sizeof(rpcs));
           if (r != sizeof(rpcs)) {
             abort();
           }
