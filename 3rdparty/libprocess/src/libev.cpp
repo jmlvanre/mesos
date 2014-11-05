e22b009398c026cd1f8545fb66a0dbdac0a325de
jmlvanre <joris.van.remoortere@gmail.com>
2014-11-05 13:07:33 -0800

    Add missing files

---
+++ 3rdparty/libprocess/src/libev.cpp
@@ -0,0 +1,19 @@
+#include <ev.h>
+
+#include <queue>
+
+#include <stout/lambda.hpp>
+
+#include "libev.hpp"
+
+namespace process {
+
+struct ev_loop* loop = NULL;
+ev_async async_watcher;
+ev_io server_watcher;
+std::queue<ev_io*>* watchers = new std::queue<ev_io*>();
+synchronizable(watchers);
+std::queue<lambda::function<void(void)>>* functions =
+  new std::queue<lambda::function<void(void)>>();
+
+} // namespace process {
